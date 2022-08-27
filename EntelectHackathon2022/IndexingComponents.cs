7a52b05e625f9fbc936e326f374a13122c3e6730
Shammah Nhlabathi <55482521+Shamza-boss@users.noreply.github.com>
2022-08-27 13:02:20 +0200

    Reader

---
+++ EntelectHackathon2022/IndexingComponents.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntelectHackathon2022
+{
+    
+    internal class IndexingComponents
+    {
+        public static List<string> coal = new List<string>();
+        public static List<string> fish = new List<string>();
+        public static List<string> scrap = new List<string>();
+        public int numRowsC;
+        public int numRowsF;
+        public int numRowsS;
+        public string getStepALLowance(string[] data)
+        {
+            string[] stepallowanceValue = data[0].Split("=");
+
+            return stepallowanceValue[1];
+        }
+        public string[] getCoal(string[] data)
+        {
+           
+            string[] Coal = data[1].Split(",");
+            numRowsC = int.Parse(Coal[1]);
+
+            for(int i = numRowsC; i< numRowsC; i++)
+            {
+                    coal.Add(data[i]);
+                
+            }
+
+            return coal.ToArray();
+        }
+        public string[] getFish(string[] data)
+        {
+            
+            string[] Fish = data[numRowsC+2].Split(",");
+            numRowsF = int.Parse(Fish[1]);
+
+            for (int i = numRowsF; i < numRowsF; i++)
+            {
+                    fish.Add(data[i]);
+
+            }
+
+            return fish.ToArray();
+        }
+        public string[] getScrap_Metal(string[] data)
+        {
+
+            string[] Scrap = data[numRowsF + 2].Split(",");
+            //Console.WriteLine(Scrap[0]);
+            numRowsS = int.Parse(scrap[0]);
+
+            for (int i = numRowsS; i < numRowsS; i++)
+            {
+                    scrap.Add(data[i]);
+            }
+            return scrap.ToArray();
+        }
+
+        public string[] getQuota(string[] data)
+        {
+            string[] Quota = data[numRowsS + 2].Split("=");
+            return Quota;
+        }
+        public string[] getQuotaM(string[] data)
+        {
+            string[] QuotaM = data[numRowsS + 3].Split("=");
+            return QuotaM;
+        }
+        public string[] getMapSize(string[] data)
+        {
+            List<string> values = new List<string>();
+            string[] MapSize = data[numRowsS + 4].Split("=");
+            values.Add(MapSize[0].Split(",")[0]);
+            values.Add(MapSize[0].Split(",")[1]);
+
+
+            return values.ToArray();
+        }
+        public string[] getMap(string[] data)
+        {
+            List<string> values = new List<string>();
+            string[] MapSize = data[numRowsS + 4].Split("=");
+            values.Add(MapSize[0].Split(",")[0]);
+            values.Add(MapSize[0].Split(",")[1]);
+            string[] mapD = values.ToArray();
+            int MapRow = int.Parse(mapD[0]);
+            int MapCol = int.Parse(mapD[1]);
+            int Start = numRowsS + 5;
+            List<string> Map = new List<string>();
+            for (int s = Start; s < MapRow; s++)
+            {
+                Map.Add("" + data[s].Split(","));
+            }
+
+
+            //List<string> Map = new List<string>();
+            //for (int i = ; i < MapRow; i++)
+            //{
+            //    for (int z = i; z < MapCol; z++)
+            //    {
+            //        Map.Add("(" + i + "," + z + ")");
+            //    }
+            //}
+            //string[] map = new string[2] { Map.ToArray(), Row.ToArray() };
+            //string[] MapSize = data[numRowsS + 5];
+            return Map.ToArray();
+        }
+    }
+}
--- EntelectHackathon2022/Program.cs
+++ EntelectHackathon2022/Program.cs
@@ -6,7 +6,31 @@
         {
             FileReader fileReader = new FileReader();
             string[] MapData = fileReader.Read();
-            Console.WriteLine(MapData);
+            //for(int i = 0; i < MapData.Length; i++)
+            //{
+            //    Console.WriteLine(MapData[i]);
+            //}
+            
+
+            IndexingComponents indexingComponents = new IndexingComponents();
+            string stepAll = indexingComponents.getStepALLowance(MapData);
+            string[] Coal = indexingComponents.getCoal(MapData);
+            string[] Fish = indexingComponents.getFish(MapData);
+            string[] scrap = indexingComponents.getScrap_Metal(MapData);
+            string[] Quaota = indexingComponents.getQuota(MapData);
+            string[] QuoataM = indexingComponents.getQuotaM(MapData);
+            string[] mapSize = indexingComponents.getMapSize(MapData);
+            //string[] Map = indexingComponents.getMap(MapData);
+
+            //string 
+            Console.WriteLine(stepAll);
+            //Console.WriteLine(Coal[0]);
+            //Console.WriteLine(Fish[0]);
+            //Console.WriteLine(scrap[0]);
+            //Console.WriteLine(Quaota[0]);
+            //Console.WriteLine(QuoataM[0]);
+            //Console.WriteLine(mapSize[0]);
+            //Console.WriteLine(Map[0]);
         }
     }
 }
