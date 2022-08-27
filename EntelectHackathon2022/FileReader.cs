b565af342638e62c9b68d2d1bb9b99904eea9ee9
Shammah Nhlabathi <55482521+Shamza-boss@users.noreply.github.com>
2022-08-27 10:48:45 +0200

    gitignore

---
+++ EntelectHackathon2022/FileReader.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntelectHackathon2022
+{
+    internal class FileReader
+    {
+        private string[] important = new string[6];
+        //The map folder containing the txt file is in the binary folder in debug
+        public static string path = "./Map/";
+
+        public string[] Read()
+        {
+            List<string> list = new List<string>();
+            //string to store correct data.
+
+            string[] files = System.IO.Directory.GetFiles(path, "*.txt");
+            // Open the text file using a stream reader.
+            using (var sr = new StreamReader(files[0]))
+            {
+                var fullText = sr.ReadToEnd();
+                var lineBreakes = fullText.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+                // Read the stream as a string, and write the string to the console.
+                //Console.WriteLine(fullText);
+                for (int i = 0; i < lineBreakes.Length; i++)
+                {
+                    if (lineBreakes[i].Length > 0)
+                    {
+                        list.Add(lineBreakes[i]);
+                    }
+                    else
+                    {
+                        //Console.WriteLine(lineBreakes[i] + "empty");
+                    }
+
+                }
+                //Adding list items to array
+                for (int i = 0; i < list.Count; i++)
+                {
+                    //Console.WriteLine(list[i]);
+                    important[i] = list[i];
+                }
+            }
+            //Console.WriteLine(important.Length);
+            return important;
+        }
+    }
+}
